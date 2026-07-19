e76e63e1e401f1dc50e0c1a19190c1a3e42e845e
DmitriiKud <dmitrii.kudriavtcev@gmail.com>
2026-07-19 05:43:13 +0300

    Знакомство с API клиентами

---
+++ clients/users/public_users_client.py
@@ -0,0 +1,29 @@
+from typing import TypedDict
+from httpx import Response
+from clients.api_client import APIClient
+
+
+class CreateUserDict(TypedDict):
+    """
+    Описание структуры запроса на создание пользователя.
+    """
+    email: str
+    password: str
+    lastname: str
+    firstname: str
+    middlename: str
+
+
+class PublicUsersClient(APIClient):
+    """
+    Клиент для работы с /api/v1/authentication
+    """
+
+    def create_user_api(self, request: CreateUserDict) -> Response:
+        """
+        Метод для создания пользователя.
+
+        :param request: Словарь с телом для создания пользователя.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return self.post("/api/v1/users", json=request)
