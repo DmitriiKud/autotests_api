9aba27b527118e0f82bd9c32e64ec0630cf80822
DmitriiKud <dmitrii.kudriavtcev@gmail.com>
2026-08-30 11:59:36 +0300

    Фикстуры Pytest

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,15 +1,20 @@
 from http import HTTPStatus
 
-from clients.users.public_users_client import get_public_users_client
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+import pytest
+
+from clients.users.private_users_client import get_private_users_client, PrivateUsersClient
+from clients.users.public_users_client import get_public_users_client, PublicUsersClient
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
+from pydantic_example import User
+from tests.conftest import UserFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
-from tools.assertions.users import assert_create_user_response
+from tools.assertions.users import assert_create_user_response, assert_get_user_response
 
 
-def test_create_user():
-    # Инициализируем API-клиент для работы с пользователями
-    public_users_client = get_public_users_client()
+@pytest.mark.users
+@pytest.mark.regression
+def test_create_user(public_users_client: PublicUsersClient):
 
     # Формируем тело запроса на создание пользователя
     request = CreateUserRequestSchema()
@@ -21,9 +26,20 @@ def test_create_user():
 
     # Проверяем статус-код ответа
     assert_status_code(response.status_code, HTTPStatus.OK)
-
     # Проверяем, что данные ответа совпадают с данными запроса
     assert_create_user_response(request, response_data)
 
     # Проверяем, что тело ответа соответствует ожидаемой JSON-схеме
+    validate_json_schema(response.json(), response_data.model_json_schema())
+
+
+@pytest.mark.users
+@pytest.mark.regression
+def test_get_user_me(function_user: UserFixture, private_users_client: PrivateUsersClient):
+    response = private_users_client.get_user_me_api()
+    response_data = GetUserResponseSchema.model_validate_json(response.text)
+
+    assert_status_code(response.status_code, HTTPStatus.OK)
+    assert_get_user_response(response_data, function_user.response)
+
     validate_json_schema(response.json(), response_data.model_json_schema())
